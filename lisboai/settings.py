5ab8b65e3143bfe8364a5c942947951eccc65ce4
Thiago Veloso <thiago.veloso@LIS-MBP-0808.local>
2019-01-24 14:15:25 +0000

    config settings

--- lisboai/settings.py
+++ lisboai/settings.py
@@ -14,6 +14,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import django_heroku
 from dj_database_url import parse as db_url
+from decouple import config
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
