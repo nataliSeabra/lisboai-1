4e448feb3d48856689278e36b676b2ff9905be86
Thiago Veloso <thiago.veloso@LIS-MBP-0808.local>
2019-01-30 17:51:59 +0000

    all

--- lisboai/core/admin.py
+++ lisboai/core/admin.py
@@ -1,1 +1,25 @@
 from django.contrib import admin
+from lisboai.core.models import Categories,Article
+
+
+# Register your models here.
+
+
+class CategoriesAdmin(admin.ModelAdmin):
+    fields = ('name', 'active')
+    list_display = ('name', 'active')
+admin.site.register(Categories, CategoriesAdmin)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+
+    fields = ('categories', 'short_description', 'description','state')
+    list_display = ('categories', 'short_description','state')
+admin.site.register(Article, ArticleAdmin)
+
+"""
+class SocialAdmin(admin.ModelAdmin):
+    fields = ('name', 'link')
+    list_display = ('name', 'link')
+admin.site.register(Social, SocialAdmin)
+""" 
--- lisboai/core/models.py
+++ lisboai/core/models.py
@@ -2,3 +2,51 @@ from django.db import models
 
 # Create your models here.
 
+# retorno para status 
+STATUS_CHOICES = (
+    ('d', 'Rascunho'),
+    ('p', 'Publicado'),    
+)
+
+ACTIVE_CHOICES = (
+    ('a', 'Ativo'),
+    ('d', 'Inativo'),    
+)
+
+
+class Categories(models.Model):
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=100,blank=True)
+    active = models.CharField(max_length=1, choices=ACTIVE_CHOICES)
+
+    def __unicode__(self): 
+        return (self.name)
+
+    def __str__(self):
+        return self.name    
+    
+
+
+class Article(models.Model):
+
+    categories = models.ForeignKey('Categories',on_delete=False)
+    short_description = models.CharField(max_length=100)
+    description = models.TextField(max_length=100,blank=True)
+    state = models.CharField(max_length=1, choices=STATUS_CHOICES)
+
+    def __unicode__(self): 
+        return (self.short_description)
+
+    def __str__(self):
+        return self.short_description      
+
+"""
+class Social(models.Model):
+
+    name = models.CharField(max_length=100)
+    link = models.URLField(max_length=200, blank=True)
+
+    def __unicode__(self): 
+        return (self.name)
+"""
