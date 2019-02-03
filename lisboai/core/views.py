15cc30b3d40a3a1ba3d818b181e3541b2e630966
Natali Seabra <nataliseabra@gmail.com>
2019-02-03 20:20:49 -0200

    created new templates and routes

--- lisboai/core/views.py
+++ lisboai/core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from lisboai.core.models import Article
 
 # Create your views here.
+
+def home(request,template_name="index.html"):
+
+    obj = Article.objects.all()
+    
+    return render(request,template_name,{"data":obj})
+    
+    
+
+def about(request,template_name="about.html"):
+
+    return render(request,template_name)
+
